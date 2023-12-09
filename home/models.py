fb2eefe8a8bbd632fa9b6d5ff5e8f9c3ca527f40
brightigiemokha <brightigiemokha@yahoo.com>
2023-12-09 08:49:37 +0000

    customised admin

--- home/models.py
+++ home/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -11,7 +15,8 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
-class product(models.Model):
+
+class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
